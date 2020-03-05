b0c1fedb628a5837dcc9670a4414f3f0fbf9153d
tomimarkus991 <tomimarkusalber@gmail.com>
2020-03-05 16:08:48 +0200

    Add webpack and Babel

--- src/app/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './style/style.css'
-
-ReactDOM.render(<App />, document.getElementById('root'));
