3cc33b2fe2cfde78c886cd133527008db5011f94
Tomi Markus Alber <tomimarkus.alber@tptlive.ee>
2020-03-16 18:42:50 +0200

    made webpack config better, made code better overall

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import Test from "./app/Test";
+import App from './app/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app/style/tailwind.css';
+
+ReactDOM.render(<App />, document.getElementById('index'));
